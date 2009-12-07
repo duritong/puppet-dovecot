f78a7b1d773b512b02f7b914c452660f38cc15f9
mh <mh@immerda.ch>
2009-12-07 22:51:54 +0100

    fix log path

--- manifests/deliver.pp
+++ manifests/deliver.pp
@@ -1,7 +1,7 @@
 class dovecot::deliver {
   include ::dovecot
-  file{ [ 'var/log/dovecot/deliver.log',
-        'var/log/dovecot/deliver-error.log' ]:
+  file{ [ '/var/log/dovecot/deliver.log',
+        '/var/log/dovecot/deliver-error.log' ]:
       require => Package['dovecot'],
       before => Service['dovecot'],
       owner => root, group => 12, mode => 0660;

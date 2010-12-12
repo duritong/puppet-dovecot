3c4a0c0b82a619986d01418525779f5c1cfde5af
mh <mh@immerda.ch>
2010-12-12 18:11:50 +0100

    improve managesieve
    * add class parameters for various things
    * if type is proxy open outgoing connections
    * add nagios checks for managesieve port

--- manifests/managesieve.pp
+++ manifests/managesieve.pp
@@ -1,10 +1,25 @@
-class dovecot::managesieve {
+class dovecot::managesieve(
+  $type = 'some_unknown_type',
+  $manage_shorewall = true,
+  $check_nagios = {
+    'hostname' => $fqdn,
+  }
+) {
   package{'dovecot-managesieve':
     ensure => installed,
     before => Service['dovecot'],
   }
 
-  if $use_shorewall {
+  if $dovecot::managesieve::manage_shorewall {
     include shorewall::rules::managesieve
+    if $dovecot::managesieve::type == 'proxy' {
+      include shorewall::rules::out::managesieve
+    }
+  }
+
+  if $dovecot::managesieve::check_nagios {
+    nagios::serve{"managesieve":
+      check_command => "check_tcp!${dovecot::managesieve::check_nagios['hostname]}!2000";
+    }
   }
 }

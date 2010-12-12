be9829660e43e56ac903a9b04911d2f2e1b10b27
mh <mh@immerda.ch>
2010-12-12 18:14:44 +0100

    simplify check hostname setting

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,8 @@ class dovecot(
   $pgsql = false,
   $mysql = false,
   $nagios_checks = {
-    'imap-hostname' => 'fqdn',
-    'pop3-hostname' => 'fqdn',
+    'imap-hostname' => $fqdn,
+    'pop3-hostname' => $fqdn,
   },
   $munin_checks = true,
   $manage_shorewall = true
@@ -34,25 +34,15 @@ class dovecot(
   }
 
   if $dovecot::nagios_checks {
-    if $dovecot::nagios_checks['imap-hostname'] == 'fqdn' {
-      $imap_host_to_check = $fqdn
-    } else {
-      $imap_host_to_check = $dovecot::nagios_checks['imap-hostname']
-    }
-    if $dovecot::nagios_checks['pop3-hostname'] == 'fqdn' {
-      $pop3_host_to_check = $fqdn
-    } else {
-      $pop3_host_to_check = $dovecot::nagios_checks['pop3-hostname']
-    }
     nagios::service{
       "check_imap":
-        check_command => "check_imap!${imap_host_to_check}!143";
+        check_command => "check_imap!${dovecot::nagios_checks['imap-hostname']}!143";
       "check_imap_ssl":
-        check_command => "check_imap_ssl!${imap_host_to_check}!993";
+        check_command => "check_imap_ssl!${dovecot::nagios_checks['imap-hostname']}!993";
       "check_pop3":
-        check_command => "check_pop3!${pop3_host_to_check}!110";
+        check_command => "check_pop3!${dovecot::nagios_checks['pop3-hostname']}!110";
       "check_pop3_ssl":
-        check_command => "check_pop3_ssl!${pop3_host_to_check}!995";
+        check_command => "check_pop3_ssl!${dovecot::nagios_checks['pop3-hostname']}!995";
     }
   }
 }

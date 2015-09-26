ff0e4a4f5fbf1c5f820565e22c428200e81b6ad3
mh <mh@immerda.ch>
2015-09-26 12:56:21 +0200

    make source configurable

--- manifests/quota.pp
+++ manifests/quota.pp
@@ -1,8 +1,10 @@
 # manages quota warning
-class dovecot::quota {
+class dovecot::quota(
+  $source = ["puppet:///modules/${dovecot::site_source}/quota/quota-warning.sh",
+              'puppet:///modules/dovecot/quota/quota-warning.sh' ],
+) {
   file{'/usr/libexec/dovecot/quota-warning.sh':
-    source  => ["puppet:///modules/${dovecot::site_source}/quota/quota-warning.sh",
-                'puppet:///modules/dovecot/quota/quota-warning.sh' ],
+    source  => $source,
     require => Package['dovecot'],
     before  => Service['dovecot'],
     owner   => root,

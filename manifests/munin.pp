56577eacce40bbbc3390c0d00a4a348f2ec65480
mh <mh@immerda.ch>
2015-09-26 13:24:17 +0200

    we now only log to syslog

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,29 +1,11 @@
 # configure munin plugins
 class dovecot::munin {
-
   munin::plugin::deploy{'dovecot':
     source => 'dovecot/munin/dovecot',
+    config => "env.logfile /var/log/maillog\nuser root",
   } -> file{'/var/lib/munin/plugin-state/plugin-dovecot.state':
-    ensure => file,
+    owner  => root,
+    group  => 0,
     mode   => '0660';
-  }
-  if $dovecot::use_syslog {
-    Munin::Plugin::Deploy['dovecot']{
-      config => 'env.logfile /var/log/maillog
-user root'
-    }
-    File['/var/lib/munin/plugin-state/plugin-dovecot.state']{
-      owner  => root,
-      group  => 0,
-    }
-  } else {
-    Munin::Plugin::Deploy['dovecot']{
-      config => 'env.logfile /var/log/dovecot/infos.log
-group mail'
-    }
-    File['/var/lib/munin/plugin-state/plugin-dovecot.state']{
-      owner  => munin,
-      group  => mail,
-    }
   }
 }

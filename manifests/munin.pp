c50841de4ef4c0b7fed67864e01dae8bed8c39c5
mh <mh@immerda.ch>
2022-08-05 11:27:29 +0200

    linting

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -4,8 +4,8 @@ class dovecot::munin {
     source => 'dovecot/munin/dovecot',
     config => "env.logfile /var/log/maillog\nuser root",
   } -> file{'/var/lib/munin-node/plugin-state/plugin-dovecot.state':
-    owner  => root,
-    group  => 0,
-    mode   => '0660';
+    owner => root,
+    group => 0,
+    mode  => '0660';
   }
 }

ba38acf7a0210bfd82c360aba8e57f1c3cf957b7
mh <mh@immerda.ch>
2022-08-05 11:27:16 +0200

    update state location

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -3,7 +3,7 @@ class dovecot::munin {
   munin::plugin::deploy{'dovecot':
     source => 'dovecot/munin/dovecot',
     config => "env.logfile /var/log/maillog\nuser root",
-  } -> file{'/var/lib/munin/plugin-state/plugin-dovecot.state':
+  } -> file{'/var/lib/munin-node/plugin-state/plugin-dovecot.state':
     owner  => root,
     group  => 0,
     mode   => '0660';
